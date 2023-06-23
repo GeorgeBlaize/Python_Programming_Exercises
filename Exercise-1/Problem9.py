1cc2d0f4052eb64fc1c35fc163f7d3974699c233
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-06-23 23:53:53 +0600

    Create Problem9.py

---
+++ Exercise-1/Problem9.py
@@ -0,0 +1,9 @@
+
+
+# Python Program to Convert Celsius To Fahrenheit
+
+celsius = float(input("Enter value in celsius:"))
+
+fahrenheit=(celsius*1.8)+32
+
+print("%0.1f degree Celsius is equal to %0.1f degree Fahrenheit"%(celsius,fahrenheit))
