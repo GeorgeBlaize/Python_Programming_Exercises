ceb87a13f3cf7d13f22bd6c5a2b7c52903ceb9d8
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-06-24 22:33:09 +0600

    Create Problem14.py

---
+++ Exercise-1/Problem14.py
@@ -0,0 +1,18 @@
+
+# Python Program to Check Prime Number
+
+num = int(input("Enter a number: "))
+
+if num == 1:
+   print(num, "is not a prime number")
+elif num > 1:
+    flag = False
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
