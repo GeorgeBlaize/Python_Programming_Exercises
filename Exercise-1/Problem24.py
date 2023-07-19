c599ee155c0f46b58057b4a299fb1cd849398fb3
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-07-20 02:57:18 +0600

    Create Problem24.py

---
+++ Exercise-1/Problem24.py
@@ -0,0 +1,10 @@
+
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+
+print("Enter decimal number:")
+number =int(input())
+
+
+print(bin(number),"in binary.")
+print(oct(number),"in octal.")
+print(hex(number),"in hexadecimal.")
