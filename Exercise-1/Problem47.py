d4ec3b6eed2a14f4f6d4a87c77fdbca2a800cc52
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-07-31 23:02:34 +0600

    Create Problem47.py

---
+++ Exercise-1/Problem47.py
@@ -0,0 +1,10 @@
+
+# Python Program to Create Pyramid Patterns
+
+rows=int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("*",end=" ")
+
+    print("\n")
