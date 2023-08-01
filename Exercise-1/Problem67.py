88ae98710de1a57b90dfbeefe04703344348db1e
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-01 23:50:37 +0600

    Create Problem67.py

---
+++ Exercise-1/Problem67.py
@@ -0,0 +1,11 @@
+
+# Python Program Read a File Line by Line Into a List
+
+with open("data_file.txt") as f:
+    content_list = f.readlines()
+
+print(content_list)
+
+content_list=[x.strip() for x in content_list]
+
+print(content_list)
