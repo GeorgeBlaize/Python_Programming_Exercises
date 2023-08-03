fb6f7c5ce534e1a1965c1652c256d9c8392b14bd
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-03 20:21:54 +0600

    Create Problem94.py

---
+++ Exercise-1/Problem94.py
@@ -0,0 +1,13 @@
+
+# Python Program to Compute all the Permutation of the String
+def get_permutation(string,i=0):
+    if i==len(string):
+        print("".join(string))
+    for j in range(i,len(string)):
+        words=[c for c in string]
+
+        words[i],words[j]=words[j],words[i]
+
+        get_permutation(words,i+1)
+
+print(get_permutation('yup'))
