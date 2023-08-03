51d094e21dd4881dad64fbfb0af2e9fa8557c832
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-03 13:24:02 +0600

    Create Problem83.py

---
+++ Exercise-1/Problem83.py
@@ -0,0 +1,10 @@
+
+# Python Program to Get Line Count of a File
+
+def file_len(fname):
+    with open(fname) as f:
+        for i,l in enumerate(f):
+            pass
+        return i+1
+
+print(file_len("my_file1.txt"))
