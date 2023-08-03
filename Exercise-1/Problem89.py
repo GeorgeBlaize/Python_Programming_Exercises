9b40881cd3fe91645adcb6dea963ef9b27b3536a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-03 13:38:18 +0600

    Create Problem89.py

---
+++ Exercise-1/Problem89.py
@@ -0,0 +1,12 @@
+
+# Python Program to Reverse a Number
+
+num=int(input("Enter an integer number:"));
+reversed_num=0
+
+while num!=0:
+    digit=num%10
+    reversed_num=reversed_num*10+digit
+    num//=10
+
+print("Reversed Number: "+str(reversed_num))
