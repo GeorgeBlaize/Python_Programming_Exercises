c1cd15c63b814eb1bf1c5ec9c5b695312839dd0a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-03 13:28:17 +0600

    Create Problem85.py

---
+++ Exercise-1/Problem85.py
@@ -0,0 +1,7 @@
+
+# Python Program to Get File Creation and Modification Date
+import os.path, time
+
+file = pathlib.Path('abc.py')
+print("Last modification time: %s" % time.ctime(os.path.getmtime(file)))
+print("Last metadata change time or path creation time: %s" % time.ctime(os.path.getctime(file)))
