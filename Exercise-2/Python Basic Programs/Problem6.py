c591735e799d259354051cc096842595540b1185
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-05 23:47:59 +0600

    Create Problem6.py

---
+++ Exercise-2/Python Basic Programs/Problem6.py
@@ -0,0 +1,9 @@
+
+# Python Program to Check Vowel or Consonant
+
+ch=input("Enter a character: ")
+
+if(ch=="A"or ch=="E" or ch=="I" or ch=="O" or ch=="U" or ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u"):
+    print(ch,"is a Vowel")
+else:
+    print(ch,"is a Consonant")
