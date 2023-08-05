ffc380649cee020fd202fabce719ea8d691f669c
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-05 23:52:30 +0600

    Create Problem1.py

---
+++ Exercise-2/Number Programs/Problem1.py
@@ -0,0 +1,8 @@
+
+# Python Program to Add Two Numbers
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number:"))
+
+sum=num1+num2
+
+print("The sum of given numbers is: ",sum)
