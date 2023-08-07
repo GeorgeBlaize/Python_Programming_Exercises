31eb51465fceb6ff8984f2a6a858ff8542c45728
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-07 22:06:34 +0600

    Create Problem4.py

---
+++ Exercise-2/Number Programs/Problem4.py
@@ -0,0 +1,13 @@
+
+# Python Program to Check if a Number is Positive Negative or Zero
+
+number=int(input("Enter number: "))
+
+if(number==0):
+    print("Number is zero")
+elif(number>0):
+    print("Number is positive")
+elif(number<0):
+    print("Number is negative")
+else:
+    print("The input is not a number")
