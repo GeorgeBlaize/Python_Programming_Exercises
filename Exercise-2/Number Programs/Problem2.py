9e6c2979edae5c2334be0367c7888d96b5aedd47
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-07 21:55:23 +0600

    Create Problem2.py

---
+++ Exercise-2/Number Programs/Problem2.py
@@ -0,0 +1,12 @@
+
+# Python Program to Check If number is Even or Odd
+
+num=int(input("Enter any number: "))
+
+if(num%2==0):
+    print(num,"is an even number")
+elif (num==0):
+    print(num,"is zero")
+
+else:
+    print(num,"is an odd number")
