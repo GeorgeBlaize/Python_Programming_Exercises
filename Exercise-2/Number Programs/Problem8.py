18fd149da75af4543bdc594e464bc7375195ad9a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-07 22:30:09 +0600

    Create Problem8.py

---
+++ Exercise-2/Number Programs/Problem8.py
@@ -0,0 +1,15 @@
+
+# Python Program to Swap Two Numbers
+
+number1=input("Enter first number: ")
+number2=input("Enter second number:")
+
+print("Value of number1 before swapping: ",number1)
+print("Value of number2 before swapping: ",number2)
+
+temp=number1
+number1=number2
+number2=temp
+
+print("Value of number1 after swapping: ",number1)
+print("Value of number2 after swapping: ",number2)
