6599056c69f33b9095589000e39b2c494248be9a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-07 21:57:10 +0600

    Update Problem2.py

--- Exercise-2/Number Programs/Problem2.py
+++ Exercise-2/Number Programs/Problem2.py
@@ -3,10 +3,10 @@
 
 num=int(input("Enter any number: "))
 
-if(num%2==0):
-    print(num,"is an even number")
-elif (num==0):
+if(num==0):
     print(num,"is zero")
+elif (num%2==0):
+    print(num,"is an even number")
 
 else:
     print(num,"is an odd number")
