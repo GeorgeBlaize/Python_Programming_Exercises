471047e08fc752380dd759b03f491c412a8688ca
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-09 00:39:08 +0600

    Create Problem4.py

---
+++ Exercise-2/Conversion Programs/Problem4.py
@@ -0,0 +1,10 @@
+
+# Python Program to Convert Kilometers(km) to Miles(mi.)
+
+km=int(input("Enter the value in kilometers: "))
+
+ratio=0.621371
+
+mi=km * ratio
+
+print("The entered value in Miles: ",mi)
