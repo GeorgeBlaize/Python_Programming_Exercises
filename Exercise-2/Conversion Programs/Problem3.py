30e68c495464e2a2b2666560f55c7b698e17ec8c
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-09 00:35:45 +0600

    Create Problem3.py

---
+++ Exercise-2/Conversion Programs/Problem3.py
@@ -0,0 +1,8 @@
+
+# Program for decimal to hexadecimal conversion
+
+decimal=int(input("Enter the decimal value for conversion: "))
+
+hex=hex(decimal)
+
+print("The equivalent hexadecimal value is: ",hex)
