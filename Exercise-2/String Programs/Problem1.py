814185fc56dd2e0fa68c3ed109f2e8d89ebba856
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-09 00:12:37 +0600

    Create Problem1.py

---
+++ Exercise-2/String Programs/Problem1.py
@@ -0,0 +1,13 @@
+
+# Python Program to Calculate length of a String
+
+str=input("Enter a string: ")
+
+counter=0
+
+for s in str:
+
+     counter = counter+1
+
+print("Length of the input string is:",counter)
+
