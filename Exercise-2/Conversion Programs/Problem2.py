b62d22b9625d73df82c5b6decba40f24355a6e6a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-08-09 00:32:48 +0600

    Create Problem2.py

---
+++ Exercise-2/Conversion Programs/Problem2.py
@@ -0,0 +1,8 @@
+
+# Program to Convert Fahrenheit to Celsius
+
+fahrenheit=float(input("Enter temperature in fahrenheit: "))
+
+celsius=(fahrenheit-32)*5/9
+
+print("%.2f Fahrenheit is: %0.2f Celsius"%(fahrenheit,celsius))
